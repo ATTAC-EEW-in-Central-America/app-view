f87bfd408eefc10399740f3b059878930c74a358
Billy Burgoa Rosso <billyburgoa@yahoo.com>
2025-08-04 10:41:51 -0400

    using global config file

--- dashboard_silent.py
+++ dashboard_silent.py
@@ -30,7 +30,7 @@ from os import path
 # Read the JSON file and load the database path
 def load_db_path():
     script_dir = path.dirname(path.abspath(__file__))
-    config_path = path.join(script_dir, 'dashboard_silent.json')
+    config_path = path.join(script_dir, 'config.json')
     with open(config_path) as config_file:
         config = json.load(config_file)
     return config['database_path']
